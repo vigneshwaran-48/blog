18f4279b632caa2ac3203b2895a95e6f2a2b9b40
vigneshwaran48 <p3487260@gmail.com>
2024-01-23 22:02:12 +0530

    Added user adding popup

--- src/app/blog/organization/edit/[id]/members/MembersEditPage.tsx
+++ src/app/blog/organization/edit/[id]/members/MembersEditPage.tsx
@@ -6,6 +6,7 @@ import React, { useState } from 'react';
 import styles from "./page.module.css";
 import OrganizationUserContainer from './OrganizationUserContainer';
 import Link from 'next/link';
+import OrganizationMemberAddPage from './OrganizationMemberAddPage';
 
 interface Props {
     users: UserMeta[],
@@ -17,9 +18,19 @@ const MembersEditPage = ({ users, orgUsers, organizationId}: Props) => {
 
     const [ allUsers, setAllUsers ] = useState<UserMeta[]>(users);
     const [ organizationUsers, setOrganizationUsers ] = useState<OrganizationUser[]>(orgUsers);
+
+    const [ openUserAddComp, setOpenUserAddComp ] = useState<boolean>(false);
     
     const handleUsersSearch = (query: String) => {
 
+    }
+
+    const handleUsersAdd = (users: UserMeta[]) => {
+
+    }
+
+    const handleOpenAddSection = () => {
+        setOpenUserAddComp(true);
     }
 
     const orgUsersElem = organizationUsers.length > 0 ? organizationUsers.map((user, key) => {
@@ -27,14 +38,24 @@ const MembersEditPage = ({ users, orgUsers, organizationId}: Props) => {
     }) : <h1>No users</h1>
 
     return (
-        <div className={`full-body y-axis-flex`}>
-            <div className={`${styles.searchWrapper} x-axis-flex full-width`}>
-                <SearchBar onSearch={handleUsersSearch} />
-                <button className={`button`}>Add</button>
+        <div className={`${styles.membersEditPage} full-width y-axis-flex`}>
+            <div className={`${styles.orgMembersListContainer} full-body`}>
+                <div className={`${styles.searchWrapper} x-axis-flex full-width`}>
+                    <SearchBar onSearch={handleUsersSearch} />
+                    <button className={`button`} onClick={e => setOpenUserAddComp(true)}>Add</button>
+                </div>
+                <div className={`full-width y-axis-flex`}>
+                    { orgUsersElem }
+                </div>
             </div>
-            <div className={`y-axis-flex`}>
-                { orgUsersElem }
-            </div>
+            {
+                openUserAddComp ? <OrganizationMemberAddPage 
+                                        members={users} 
+                                        onAdd={handleUsersAdd} 
+                                        onClose={() => setOpenUserAddComp(false)}
+                                    /> 
+                                : ""
+            }
         </div>
     );
 }
