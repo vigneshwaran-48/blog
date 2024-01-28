d39ff7d3c6196240bc0058e018016b25863f8c96
vigneshwaran48 <p3487260@gmail.com>
2024-01-28 22:02:36 +0530

    Using React quill for Rich text editor and back to React strict mode

--- next.config.js
+++ next.config.js
@@ -2,8 +2,7 @@
 const nextConfig = {
     images: {
         remotePatterns: [{ hostname: "localhost" }, { hostname: "lh3.googleusercontent.com" }, { hostname: "192.168.172.188" }]
-    },
-    reactStrictMode: false
+    }
 }
 
 module.exports = nextConfig
--- src/app/blog/compose/BlogComposeComp.tsx
+++ src/app/blog/compose/BlogComposeComp.tsx
@@ -1,144 +1,25 @@
 "use client";
 
 import React, { useEffect, useRef, useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 import styles from "./compose.module.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faImage } from '@fortawesome/free-solid-svg-icons';
 
-interface LineProps {
-    index: number,
-    text: string,
-    onEnterCallback: (index: number) => void,
-    focus?: boolean,
-    onTextChange: (index: number, text: string) => void
-}
-
-const ParaLine = ({ index, text, onEnterCallback, focus = false, onTextChange }: LineProps) => {
-
-    const pRef = useRef<HTMLParagraphElement>(null);
-
-    useEffect(() => {
-        if(focus && pRef.current) {
-            pRef.current.focus();
-        }
-    }, []);
-
-    return (
-        <p
-            ref={pRef}
-            contentEditable={true}
-            onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                    e.preventDefault();
-                    onEnterCallback(index);
-                }
-            }}
-            onBlur={e => onTextChange(index, e.currentTarget.textContent || "")}
-            suppressContentEditableWarning
-        >
-            { text }
-        </p>
-    );
-};  
-
-const H4Line = ({ index, text, onEnterCallback, onTextChange }: LineProps) => {
-
-    return (
-        <h4
-            contentEditable={true}
-            onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                    e.preventDefault();
-                    onEnterCallback(index);
-                }
-            }}
-            onBlur={e => onTextChange(index, e.currentTarget.textContent || "")}
-            suppressContentEditableWarning
-        >
-            { text }
-        </h4>
-    );
-};
-
-interface BlogContent {
-    index: number,
-    text: string,
-    type: "p" | "h4",
-    focus?: boolean
-}
 const BlogComposeComp = () => {
 
-    const handleLineEnter = (index: number) => {
-        setContentStructure(prevStructure => {
-            const newLine: BlogContent = { type: "p", index: index + 1, text: "", focus: true };
-
-            prevStructure.forEach((line) => {
-                if (line.index >= index + 1) {
-                    line.index = line.index + 1;
-                }
-                line.focus = false;
-            });
-            prevStructure.push(newLine);
-
-            return [...prevStructure];
-        });
-    };
-
-    const blogContent: BlogContent[] = [
-        { type: "p", index: 1, text: "Hi" },
-        { type: "h4", index: 2, text: "Hello" }
-    ]
-
-    const [ contentStructure, setContentStructure ] = useState<BlogContent[]>(blogContent);
-
-    const handleTextChange = (index: number, text: string) => {
-        setContentStructure(prevContentStructure => {
-            return prevContentStructure.map(content => {
-                if(content.index === index) {
-                    content.text = text;
-                }
-                return content;
-            });
-        });
-    }
-
-    const content = contentStructure && contentStructure.sort((a, b) => a.index - b.index).map((line) => {
-                        if (line.type === "p") {
-                            return (
-                                <ParaLine
-                                    key={line.index}
-                                    index={line.index}
-                                    text={line.text}
-                                    onEnterCallback={handleLineEnter}
-                                    focus={line.focus || false}
-                                    onTextChange={handleTextChange}
-                                />
-                            );
-                        } else if (line.type === "h4") {
-                            return (
-                                <H4Line
-                                    key={line.index}
-                                    index={line.index}
-                                    text={line.text}
-                                    onEnterCallback={handleLineEnter}
-                                    focus={line.focus || false}
-                                    onTextChange={handleTextChange}
-                                />
-                            );
-                        }
-                        return null;
-                    });
+    const [ content, setContent ] = useState<string>("");
 
     return (
-        <div className={`${styles.composeContainer} x-axis-flex`}>
-            <div className={`${styles.composeBar} y-axis-flex`}>
-                <span>
-                    <FontAwesomeIcon icon={faImage} />
-                </span>
-            </div>
-            <div className={`${styles.composeArea}`}>
-                { content }
-            </div>
+        <div className={`${styles.composeArea}`}>
+            <ReactQuill 
+                theme="snow" 
+                value={content} 
+                onChange={setContent}
+                style={{
+                    width: "100%",
+                    height: "100%"
+                }}
+            />
         </div>
     )
 }
