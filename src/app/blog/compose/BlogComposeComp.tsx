06db737a89b5785fc12b10cdc59f52c8e50007cf
vigneshwaran48 <p3487260@gmail.com>
2024-02-06 21:51:29 +0530

    Added ck editor

--- src/app/blog/compose/BlogComposeComp.tsx
+++ src/app/blog/compose/BlogComposeComp.tsx
@@ -1,33 +1,43 @@
 "use client";
 
 import React, { useEffect, useMemo, useState } from 'react';
-import styles from "./compose.module.css";
-import { useAppDispatch, useAppSelector } from '@/lib/hooks';
+import { useAppDispatch } from '@/lib/hooks';
 import { setContent } from '@/lib/features/compose/composeSlice';
-import { Editor } from "react-draft-wysiwyg";
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import styles from "./compose.module.css";
 import { EditorState } from 'draft-js';
-import { stateToHTML } from 'draft-js-export-html';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+const editorConfiguration = {
+    toolbar: [
+        'heading',
+        '|',
+        'bold',
+        'italic',
+        'link',
+        'bulletedList',
+        'numberedList',
+        '|',
+        'outdent',
+        'indent',
+        '|',
+        'imageUpload',
+        'blockQuote',
+        'insertTable',
+        'mediaEmbed',
+        'undo',
+        'redo'
+    ]
+};
 
 const BlogComposeComp = () => {
 
     const dispatch = useAppDispatch();
 
-    const [ editorState, setEditorState ] = useState(() => EditorState.createEmpty());
-
-    useEffect(() => {
-        dispatch(setContent(stateToHTML(editorState.getCurrentContent())))
-    }, [editorState]);
-
-    return (
-        <div className={`${styles.composeArea}`}>
-            <Editor 
-                editorState={editorState}
-                onEditorStateChange={setEditorState}
-                wrapperClassName={styles.editorWrapper}
+    return <CKEditor 
+                editor={ ClassicEditor }
+                onChange={e => console.log(e)}
             />
-        </div>
-    )
 }
 
 export default BlogComposeComp;
