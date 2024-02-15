b65d5dea4a1ce6309683ec4fdb2858b2d357c541
vigneshwaran48 <p3487260@gmail.com>
2024-02-15 22:01:36 +0530

    Added pages

---
+++ src/app/blog/[...name]/[...id]/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface Props {
+    params: { id: number, name: string }
+}
+
+const page = ({ params: { id, name } }: Props) => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default page;
---
+++ src/app/blog/[...name]/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface Props {
+    params: { name: string }
+}
+
+const page = ({ params: { name } }: Props) => {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default page;
