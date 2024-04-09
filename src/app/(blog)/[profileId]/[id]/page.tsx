d0596989da9d03d24fbc0bce2398458c27186f06
vigneshwaran48 <p3487260@gmail.com>
2024-04-09 21:06:41 +0530

    Public blog changes

--- src/app/(blog)/[profileId]/[id]/page.tsx
+++ src/app/(blog)/[profileId]/[id]/page.tsx
@@ -6,6 +6,7 @@ import { Metadata } from 'next';
 import { AppFields } from '@/util/AppFields';
 import BlogPage from './components/BlogPage';
 import EmptyBlogPage from './components/EmptyBlogPage';
+import Link from 'next/link';
 
 interface Props {
     params: { id: string, profileId: string }
@@ -37,10 +38,13 @@ const page = async ({ params: { id, profileId } }: Props) => {
         content = <BlogPage profileId={profileId} blog={blog} comments={comments} likesOfBlog={likesOfBlog} />;
     } else if (blogStatus === AppFields.PageStatus.SIGNUP) {
         content = (
-            <div className="relative w-full h-full flex">
+            <div className="relative hide-scrollbar overflow-y-scroll w-full h-full flex">
                 <EmptyBlogPage className="absolute" />
-                <div className="absolute bg-[#0000004d] h-[80%] w-[100%] translate-y-[10%]">
-
+                <div className="flex justify-center items-center absolute sm:bg-[#0000004d] right-0 left-0 h-full">
+                    <div className="max-w-[500px] shadow-2xl border sm:border-none w-[95%] flex flex-col justify-around items-center bg-[--app-background-color] p-2 rounded">
+                        <p className="text-[20px] p-2 text-center">Your daily limit is reached, Signup to read countless blogs!</p>
+                        <Link href={`/auth/signin`} className="button bg-[--app-selected-background-color] text-[--app-selected-text-color] inline-block my-[10px]">Signin</Link>
+                    </div>
                 </div>
             </div>
         )
