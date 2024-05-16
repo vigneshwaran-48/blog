046d206f3343726b276c767051b8dadd7ae2f4b3
vigneshwaran48 <p3487260@gmail.com>
2024-05-16 21:36:20 +0530

    Sign in page redirect changes

--- src/app/(auth)/auth/signin/SigninPage.tsx
+++ src/app/(auth)/auth/signin/SigninPage.tsx
@@ -1,76 +1,21 @@
 "use client";
 
 import CircleLoader from '@/app/(blog)/components/loaders/CircleLoader';
+import DolphinLoader from '@/app/(blog)/components/loaders/DolphinLoader';
 import { signIn } from 'next-auth/react';
 import Image from 'next/image';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const SignupPage = ({ error, callbackUrl }: { error: string | undefined, callbackUrl: string | undefined }) => {
 
-    const [ providerData, setProviderData ] = useState([
-        {
-            message: "Continue with google",
-            icon: "/auth-icons/google.svg",
-            isLoading: false,
-            id: "vapps"
-        },
-        {
-            message: "Continue with github",
-            icon: "/auth-icons/github.svg",
-            isLoading: false,
-            id: "github"
-        },
-        {
-            message: "Continue with facebook",
-            icon: "/auth-icons/facebook.svg",
-            isLoading: false,
-            id: "facebook"
-        }
-    ]);
 
-    const handleSignin = (id: string) => {
-        setProviderData(prevData => {
-            return prevData.map(provider => {
-                if (provider.id === id) {
-                    provider.isLoading = true;
-                }
-                return provider;
-            });
-        });
-        if (callbackUrl) {
-            signIn(id, { callbackUrl })
-        } else {
-            signIn(id, { callbackUrl: `${window.location.origin}` });
-        }
-    }
-
-    const providers = providerData.map((provider, key) => {
-        return (
-            <div
-                key={key}
-                className="flex w-full mb-4 p-2 rounded-lg transition cursor-pointer items-center hover:bg-[--app-light-background-color]"
-                onClick={() => handleSignin(provider.id)}
-            >
-                <Icon link={provider.icon} />
-                <p className="ml-2 text-[22px] font-serif flex justify-center items-center w-[calc(100%-50px)]">
-                    { provider.isLoading ? <CircleLoader width={40} height={40} /> : provider.message }
-                </p>
-            </div>
-        )
-    })
+    useEffect(() => {
+        signIn("vapps", { callbackUrl })
+    }, []);
 
     return (
-        <div className="flex flex-col justify-end items-center p-4 w-full h-full">
-            {
-                error &&
-                <div className="w-3/4 border text-center border-[red] p-2 rounded-md mb-[40px]">
-                    <p className="text-[20px] text-[red]"> {error} </p>
-                </div>
-            }
-
-            <div className="w-[90%] h-3/4 sm:w-3/4">
-                { providers }
-            </div>
+        <div className="w-full h-full flex justify-center items-center">
+            <DolphinLoader />
         </div>
     )
 }
--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -14,12 +14,7 @@ const layout = ({ children }: { children: React.ReactNode }) => {
                 />
             </head>
             <body className="flex justify-center items-center p-4">
-                <main className="flex flex-col border items-center rounded-2xl overflow-hidden w-full h-full max-h-[700px] max-w-[500px]">
-                    <h1 className="text-4xl font-semibold p-2">Sign In</h1>
-                    <div className="w-full h-[calc(100%-60px)]">
-                        {children}
-                    </div>
-                </main>
+                {children}
             </body>
         </html>
     )
